c285b3d2546e21a1b23c2e93cc3e74f32b609da5
SanRoosalu <roosalu.sander@gmail.com>
2020-12-07 15:35:54 +0200

    Added block turning logic.

--- Android project/app/src/main/java/com/example/tetris/GameCanvas.kt
+++ Android project/app/src/main/java/com/example/tetris/GameCanvas.kt
@@ -23,6 +23,7 @@ class GameCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
     var activeBlockNr = 0
     var activeBlockCoords = arrayOf(0, 4)
     var activeBlock = ""
+    var mutation = ""
     val startBlockArray = arrayOf("I", "O", "T", "J", "L", "S", "Z")
     var paintArray: ArrayList<Paint> = ArrayList()
 
@@ -106,52 +107,61 @@ class GameCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private fun newPiece() {
         paintArray.add(getRandPaint())
+        val newBlock = startBlockArray[Random.nextInt(0, startBlockArray.size)]
 
-        val newBlock = Random.nextInt(0, startBlockArray.size)
-
-        if (newBlock == 0){
+        if (newBlock == "I"){
+            mutation = "I1"
             matrix[0][4] = activeBlockNr
             matrix[0][5] = activeBlockNr
             matrix[0][6] = activeBlockNr
             matrix[0][7] = activeBlockNr
         }
-        if (newBlock == 1){
+        else if (newBlock == "O"){
+            mutation = "O"
             matrix[0][4] = activeBlockNr
             matrix[0][5] = activeBlockNr
             matrix[1][4] = activeBlockNr
             matrix[1][5] = activeBlockNr
         }
-        if (newBlock == 2){
-            matrix[0][4] = activeBlockNr
+        else if (newBlock == "T"){
+            mutation = "T1"
             matrix[0][5] = activeBlockNr
-            matrix[0][6] = activeBlockNr
+            matrix[1][4] = activeBlockNr
             matrix[1][5] = activeBlockNr
+            matrix[1][6] = activeBlockNr
         }
-        if (newBlock == 3){
+        else if (newBlock == "J"){
+            mutation = "J1"
             matrix[0][5] = activeBlockNr
             matrix[1][5] = activeBlockNr
             matrix[2][5] = activeBlockNr
             matrix[2][4] = activeBlockNr
         }
-        if (newBlock == 4){
-            matrix[0][4] = activeBlockNr
-            matrix[1][4] = activeBlockNr
-            matrix[2][4] = activeBlockNr
+        else if (newBlock == "L"){
+            mutation = "L1"
+            matrix[0][5] = activeBlockNr
+            matrix[1][5] = activeBlockNr
             matrix[2][5] = activeBlockNr
+            matrix[2][6] = activeBlockNr
         }
-        if (newBlock == 5){
+        else if (newBlock == "S"){
+            mutation = "S1"
             matrix[0][5] = activeBlockNr
             matrix[0][6] = activeBlockNr
             matrix[1][4] = activeBlockNr
             matrix[1][5] = activeBlockNr
         }
-        if (newBlock == 6){
+        else if (newBlock == "Z"){
+            mutation = "Z1"
             matrix[0][4] = activeBlockNr
             matrix[0][5] = activeBlockNr
             matrix[1][5] = activeBlockNr
             matrix[1][6] = activeBlockNr
         }
-        activeBlock = startBlockArray[newBlock]
+
+        activeBlock = newBlock
+        activeBlockCoords[0] = 0
+        activeBlockCoords[1] = 4
     }
 
 
@@ -180,38 +190,36 @@ class GameCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
 
 
-
-
-
-/*
-          ***************************  REMOTE  ****************************
- */
-
-
-
-
     fun remote(command: String) {
         if(!isPaused){ //So when the isPause is true, the block couldn't be moved.
             if (command == "Left"){
                 if (checkLeft()) activeLeft()
             }
-            if (command == "Right") {
+            else if (command == "Right") {
                 if (checkRight()) activeRight()
             }
-            if (command == "Down") {
+            else if (command == "Down") {
                 if (checkDown()) {
                     activeDown()
                     score += 5
                 }
             }
-            if (command == "Up") {
-                //if (canChange()) activeChange()
+            else if (command == "Up") {
+                if (checkRotation()) activeRotate()
             }
         }
     }// remote
 
-    // "Checks": Boolean functions, which return true/false depending of whether block can be moved to
-    //           designated direction without interference, or not.
+
+
+    /*
+          ***************************  BLOCK MUTATION FUNCTIONS  ****************************
+    */
+
+
+
+    // "Checks": Boolean functions, which return true/false depending of whether block can be mutated
+    //           designated way, or not.
 
     private fun checkDown(): Boolean {
         if (matrix[15].contains(activeBlockNr)) return false
@@ -261,8 +269,88 @@ class GameCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
         return true
     }
 
+    private fun checkRotation(): Boolean {
+        val y = activeBlockCoords[1]
+        val x = activeBlockCoords[0]
+        if (activeBlock == "I"){
+            if (x > 12 || y > 6) return false
+            if (mutation == "I1"){
+                if (matrix[x+1][y] == 0 && matrix[x+2][y] == 0 && matrix[x+3][y] == 0){
+                    return true
+                }
+            }
+            else if (mutation == "I2") {
+                if (matrix[x][y+1] == 0 && matrix[x][y+2] == 0 && matrix[x][y+3] == 0){
+                    return true
+                }
+            }
+        }
+        //Because all other blocks fit into 3x3 cube, efficient
+        if (x > 13 || y > 7) return false
+        if (activeBlock == "T"){
+            if (mutation == "T1") {
+                if (matrix[x+2][y+1] == 0) return true
+            }
+            else if (mutation == "T2") {
+                if (matrix[x+1][y+2] == 0) return true
+            }
+            else if (mutation == "T3") {
+                if (matrix[x][y+1] == 0) return true
+            }
+            else if (mutation == "T4") {
+                if (matrix[x+1][y] == 0) return true
+            }
+        }
+        if (activeBlock == "J"){
+            if (mutation == "J1"){
+                if (matrix[x][y] == 0 && matrix[x+1][y] == 0 && matrix[x+1][y+2] == 0) return true
+            }
+            else if (mutation == "J2"){
+                if (matrix[x][y+1] == 0 && matrix[x][y+2] == 0 && matrix[x+2][y+1] == 0) return true
+            }
+            else if (mutation == "J3"){
+                if (matrix[x+1][y] == 0 && matrix[x+1][y+2] == 0 && matrix[x+2][y+2] == 0) return true
+            }
+            else if (mutation == "J4"){
+                if (matrix[x][y+1] == 0 && matrix[x+2][y] == 0 && matrix[x+2][y+1] == 0) return true
+            }
+        }
+        if (activeBlock == "L"){
+            if (mutation == "L1"){
+                if (matrix[x+1][y] == 0 && matrix[x+1][y+2] == 0 && matrix[x+2][y] == 0) return true
+            }
+            else if (mutation == "L2"){
+                if (matrix[x][y] == 0 && matrix[x][y+1] == 0 && matrix[x+2][y+1] == 0) return true
+            }
+            else if (mutation == "L3"){
+                if (matrix[x][y+2] == 0 && matrix[x+1][y] == 0 && matrix[x+1][y+2] == 0) return true
+            }
+            else if (mutation == "L4"){
+                if (matrix[x][y+1] == 0 && matrix[x+2][y+1] == 0 && matrix[x+2][y+2] == 0) return true
+            }
+        }
+        if (activeBlock == "S"){
+            if (mutation == "S1"){
+                if (matrix[x][y] == 0 && matrix[x+2][y+1] == 0) return true
+            }
+            else if (mutation == "S2"){
+                if (matrix[x][y+1] == 0 && matrix[x][y+2] == 0) return true
+            }
+        }
+        if (activeBlock == "Z"){
+            if (mutation == "Z1"){
+                if (matrix[x+1][y] == 0 && matrix[x+2][y] == 0) return true
+            }
+            else if (mutation == "Z2"){
+                if (matrix[x][y] == 0 && matrix[x+1][y+2] == 0) return true
+            }
+        }
+        // No turn for "O" block, because it's not needed
+        return false
+    }
 
-    // "Actives": void functions, which move the current active block to the destignated direction.
+
+    // "Actives": void functions, which mutate the current active block designated way.
 
     private fun activeDown() {
         var number: Int
@@ -275,6 +363,7 @@ class GameCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
                 }
             }
         }
+        activeBlockCoords[0] += 1
     }
 
     private fun activeLeft() {
@@ -288,6 +377,7 @@ class GameCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
                 }
             }
         }
+        activeBlockCoords[1] -= 1
     }
 
     private fun activeRight() {
@@ -299,6 +389,164 @@ class GameCanvas(context: Context, attrs: AttributeSet) : View(context, attrs) {
                     matrix[i][j] = matrix[i][j+1]
                     matrix[i][j+1] = activeBlockNr
                 }
+            }
+        }
+        activeBlockCoords[1] += 1
+    }
+
+    private fun activeRotate() {
+        val y = activeBlockCoords[1]
+        val x = activeBlockCoords[0]
+        if (activeBlock == "I"){
+            if (mutation == "I1") {
+                mutation = "I2"
+                matrix[x+1][y] = activeBlockNr
+                matrix[x+2][y] = activeBlockNr
+                matrix[x+3][y] = activeBlockNr
+                matrix[x][y+1] = 0
+                matrix[x][y+2] = 0
+                matrix[x][y+3] = 0
+            }
+            else if (mutation == "I2") {
+                mutation = "I1"
+                matrix[x][y+1] = activeBlockNr
+                matrix[x][y+2] = activeBlockNr
+                matrix[x][y+3] = activeBlockNr
+                matrix[x+1][y] = 0
+                matrix[x+2][y] = 0
+                matrix[x+3][y] = 0
+            }
+        }
+        else if (activeBlock == "T"){
+            if (mutation == "T1"){
+                mutation = "T2"
+                matrix[x+2][y+1] = activeBlockNr
+                matrix[x+1][y+2] = 0
+            }
+            else if (mutation == "T2"){
+                mutation = "T3"
+                matrix[x+1][y+2] = activeBlockNr
+                matrix[x][y+1] = 0
+            }
+            else if (mutation == "T3"){
+                mutation = "T4"
+                matrix[x][y+1] = activeBlockNr
+                matrix[x+1][y] = 0
+            }
+            else if (mutation == "T4"){
+                mutation = "T1"
+                matrix[x+1][y] = activeBlockNr
+                matrix[x+2][y+1] = 0
+            }
+
+        }
+        else if (activeBlock == "J"){
+            if (mutation == "J1"){
+                mutation = "J2"
+                matrix[x][y] = activeBlockNr
+                matrix[x+1][y] = activeBlockNr
+                matrix[x+1][y+2] = activeBlockNr
+                matrix[x][y+1] = 0
+                matrix[x+2][y] = 0
+                matrix[x+2][y+1] = 0
+            }
+            else if (mutation == "J2"){
+                mutation = "J3"
+                matrix[x][y+1] = activeBlockNr
+                matrix[x][y+2] = activeBlockNr
+                matrix[x+2][y+1] = activeBlockNr
+                matrix[x][y] = 0
+                matrix[x+1][y] = 0
+                matrix[x+1][y+2] = 0
+            }
+            else if (mutation == "J3"){
+                mutation = "J4"
+                matrix[x+1][y] = activeBlockNr
+                matrix[x+1][y+2] = activeBlockNr
+                matrix[x+2][y+2] = activeBlockNr
+                matrix[x][y+1] = 0
+                matrix[x][y+2] = 0
+                matrix[x+2][y+1] = 0
+            }
+            else if (mutation == "J4"){
+                mutation = "J1"
+                matrix[x][y+1] = activeBlockNr
+                matrix[x+2][y] = activeBlockNr
+                matrix[x+2][y+1] = activeBlockNr
+                matrix[x+1][y] = 0
+                matrix[x+1][y+2] = 0
+                matrix[x+2][y+2] = 0
+            }
+
+        }
+        else if (activeBlock == "L"){
+            if (mutation == "L1"){
+                mutation = "L2"
+                matrix[x+1][y] = activeBlockNr
+                matrix[x+1][y+2] = activeBlockNr
+                matrix[x+2][y] = activeBlockNr
+                matrix[x][y+1] = 0
+                matrix[x+2][y+1] = 0
+                matrix[x+2][y+2] = 0
+            }
+            else if (mutation == "L2"){
+                mutation = "L3"
+                matrix[x][y] = activeBlockNr
+                matrix[x][y+1] = activeBlockNr
+                matrix[x+2][y+1] = activeBlockNr
+                matrix[x+1][y] = 0
+                matrix[x+1][y+2] = 0
+                matrix[x+2][y] = 0
+            }
+            else if (mutation == "L3"){
+                mutation = "L4"
+                matrix[x][y+2] = activeBlockNr
+                matrix[x+1][y] = activeBlockNr
+                matrix[x+1][y+2] = activeBlockNr
+                matrix[x][y] = 0
+                matrix[x][y+1] = 0
+                matrix[x+2][y+1] = 0
+            }
+            else if (mutation == "L4"){
+                mutation = "L1"
+                matrix[x][y+1] = activeBlockNr
+                matrix[x+2][y+1] = activeBlockNr
+                matrix[x+2][y+2] = activeBlockNr
+                matrix[x][y+2] = 0
+                matrix[x+1][y] = 0
+                matrix[x+1][y+2] = 0
+            }
+        }
+        else if (activeBlock == "S"){
+            if (mutation == "S1"){
+                mutation = "S2"
+                matrix[x][y] = activeBlockNr
+                matrix[x+2][y+1] = activeBlockNr
+                matrix[x][y+1] = 0
+                matrix[x][y+2] = 0
+            }
+            else if (mutation == "S2"){
+                mutation = "S1"
+                matrix[x][y+1] = activeBlockNr
+                matrix[x][y+2] = activeBlockNr
+                matrix[x][y] = 0
+                matrix[x+2][y+1] = 0
+            }
+        }
+        else if (activeBlock == "Z"){
+            if (mutation == "Z1"){
+                mutation = "Z2"
+                matrix[x+1][y] = activeBlockNr
+                matrix[x+2][y] = activeBlockNr
+                matrix[x][y] = 0
+                matrix[x+1][y+2] = 0
+            }
+            else if (mutation == "Z2"){
+                mutation = "Z1"
+                matrix[x][y] = activeBlockNr
+                matrix[x+1][y+2] = activeBlockNr
+                matrix[x+1][y] = 0
+                matrix[x+2][y] = 0
             }
         }
     }
